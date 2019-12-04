be08e071ad58f7dfce47b4f47986a7a046a44c29
folkengine <gaoler@electronicpanopticon.com>
2019-12-04 18:28:25 -0500

    Set pg pool size to 2, added releases.exs info

--- config/releases.exs
+++ config/releases.exs
@@ -1,1 +1,7 @@
 import Config
+
+config :phawkes, PhawkesWeb.Endpoint,
+  server:true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+
